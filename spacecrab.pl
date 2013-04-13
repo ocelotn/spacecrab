c621820101c1cbedc43502b52e2d687b715c1be2
Lara <lortizde@gmail.com>
2013-04-13 00:42:09 -0700

    ordering files

--- spacecrab.pl
+++ spacecrab.pl
@@ -1,13 +1,14 @@
-#! /usr/bin/perl
+#! /usr/local/bin/perl
 use strict;
 use warnings;
-use SpaceCrab;
+use spacecrab;
 
 my $startnode = 0;
 
 #init
 my $nodeno = ($ARGV[0])?$ARGV[0]:$startnode;
 my $page = SpaceCrab->getPage($nodeno);
+print "Content-type:text/html\r\n\r\n";
 print $page;
 
 1;
