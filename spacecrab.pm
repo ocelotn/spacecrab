4aa961cea068c11abf3f2fc0faac20bbfa8be6d9
Lara <lortizde@gmail.com>
2013-04-23 12:12:51 -0700

    fixing issue #21 (node pattern), #22 (getCleanNode error return), and misc refactoring (simplified argument proc. for spacecrab::grabNode, better commenting for node pattern, etc.)

--- spacecrab.pm
+++ spacecrab.pm
@@ -1,4 +1,4 @@
-#!/usr/bin/env perl -wT
+#!/usr/local/bin perl -wT
 use strict;
 package SpaceCrab;
 
@@ -8,16 +8,18 @@ use lib qw(.);
 use spacecrabcfg;
 my $cfg = spacecrabcfg::config();
 
-
-
 #methods
 sub getCleanNodeno{
-	my $nodename = shift;
+	#takes a node id string as input
+	#sanity checks it for length and matching a defined node id pattern
+	#returns the node id scalar for node ids that appear valid
+	#returns undefined for node ids that appear bogus
+	my $nodename = shift; #get first parameter as name to clean
 	if (length($nodename) >= $cfg->{"maxfnamelen"}) {
-		return ""
+		return;
 	} else { 
 		$nodename =~ $cfg->{"nodepattern"};
-		return $1;
+		return $1; 
 	}
 }
 
@@ -34,10 +36,11 @@ sub grabSnippet{
 
 sub grabNode{
 	#takes and cleans a node id
-	#returns contents of the node or warning if node is invalid
-	my $nodeno = (@_ > 1)?$_[1]:$_[0];
+	#returns contents of a valid node 
+	#returns the error div if node is invalid
+	my $nodeno = pop;
 	$nodeno = getCleanNodeno($nodeno);
-	return ($nodeno ne "" && defined $nodeno)?
+	return (defined $nodeno)?
 		grabSnippet($cfg->{"storypath"}.$nodeno.$cfg->{"storysuffix"}):
 		$cfg->{"text400"};
 }
--- spacecrabcfg.pm
+++ spacecrabcfg.pm
@@ -1,26 +1,38 @@
-#!/usr/bin/env perl -wT
+#!/usr/local/bin/perl -wT
 use strict;
-
 package spacecrabcfg;
+our $VERSION = '1.01';
+use base 'Exporter';
+our @EXPORT = qw(config);
 
+	my $storysuffix = '.node';
+	
 	my $cfg = {
-	"startnode" => 0,
-	"storypath" => "testdata/story/",
-	"boilerplatepath" => "boilerplate/",
-	"headername" => "header.html",
-	"footername" => "footer.html",
-	"storysuffix" => ".node",
-	"nodepattern" => qr/(\d+)/,
-	"baseurl" => 'http://test.space-crab.com/',
-	"spacecrab" => "spacecrab.pl",
-	"spacecrabmeat" => "spacecrabmeat.pl",
-	"maxfnamelen" => 32,
-	"text400" => "<p>Hmm...That node isn't on our scanners.  Maybe it fell into a black hole?</p>\n",
-	"testdata" => "testdata/"
+		#behaviour defaults
+		"startnode" => 0,	
+		#finding files
+		"storypath" => "testdata/story/",
+		"boilerplatepath" => "boilerplate/",
+		"headername" => "header.html",
+		"footername" => "footer.html",
+		"storysuffix" => $storysuffix,
+		"spacecrab" => "spacecrab.pl",
+		"spacecrabmeat" => "spacecrabmeat.pl",
+		#node id constraints
+		"maxfnamelen" => 32,
+		"nodepattern" => qr/^(\d+)($storysuffix)?$/, 
+			#begins with one or more digits, 
+			#optionally followed by .node 
+			#but nothing else before ending
+		#player visible strings
+		"text400" => "<p>Hmm...That node isn't on our scanners.  Maybe it fell into a black hole?</p>\n",
+		#for tests
+		"testdata" => "testdata/",
+		"baseurl" => 'http://test.space-crab.com/'
 	};
 
 	sub config {
 		return $cfg;
 	}
-
+	
 1;
