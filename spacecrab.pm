3dc21a9f24bb21b59b19e4791589bde752f80ed3
Lara <lortizde@gmail.com>
2013-04-30 17:18:17 -0700

    minor formatting changes

--- spacecrab.pm
+++ spacecrab.pm
@@ -1,4 +1,9 @@
 use strict;
+use File::Slurp;
+use Mojo::DOM;
+use lib('.');
+use spacecrabcfg;
+
 package SpaceCrab;
 
 #config
@@ -9,56 +14,72 @@ my $cfg = spacecrabcfg::config();
 
 #methods
 sub getCleanNodeno{
-	#takes a node id string as input
-	#sanity checks it for length and matching a defined node id pattern
-	#returns the node id scalar for node ids that appear valid
-	#returns undefined for node ids that appear bogus
-	my $nodename = shift; #get first parameter as name to clean
-	if (length($nodename) >= $cfg->{"maxfnamelen"}) {
-		return;
-	} else { 
-		$nodename =~ $cfg->{"nodepattern"};
-		return $1; 
-	}
+   #takes a node id string as input
+   #sanity checks it for length and matching a defined node id pattern
+   #returns the node id scalar for node ids that appear valid
+   #returns undefined for node ids that appear bogus
+   my $nodename = shift; #get first parameter as name to clean
+   if (length($nodename) >= $cfg->{"maxfnamelen"}) {
+      return;
+   } else { 
+      $nodename =~ $cfg->{"nodepattern"};
+      return $1; 
+   }
 }
 
 sub grabSnippet{
-	#assumes a clean file path
-	#returns contents of file as string
-	my $filepath = $_[0];
-	open(my $fh,"<", $filepath) or return $cfg->{"text400"};
-	my $wodgeoftext;
-	while(<$fh>){$wodgeoftext.=$_;}
-    close $fh;	
-	return $wodgeoftext;
+   #assumes a clean file path
+   #returns contents of file as string
+   my $filepath = $_[0];
+   open(my $fh,"<", $filepath) or return $cfg->{"text400"};
+   my $wodgeoftext;
+   while(<$fh>){$wodgeoftext.=$_;}
+    close $fh;   
+   return $wodgeoftext;
+}
+
+sub parseNode{
+ my $dom = Mojo::DOM->new(shift);
+ my $div = $dom->find('div[class"story"]');
+ my %attributes;
+ $images{"fg"} = $dom->pluck('fg'):$cfg->{"fgdefault"};
+ $images{"mg"} = $dom->pluck('mg'):$cfg->{"mgdefault"};
+ $images{"bg"} = $dom->pluck('bg'):$cfg->{"bgdefault"};
+ return \%attributes;
 }
 
 sub grabNode{
-	#takes and cleans a node id
-	#returns contents of a valid node 
-	#returns the error div if node is invalid
-	my $nodeno = pop;
-	$nodeno = getCleanNodeno($nodeno);
-	return (defined $nodeno)?
-		grabSnippet($cfg->{"storypath"}.$nodeno.$cfg->{"storysuffix"}):
-		$cfg->{"text400"};
+   #takes and cleans a node id
+   #returns contents of a valid node 
+   #returns the error div if node is invalid
+   my $nodeno = pop;
+   $nodeno = getCleanNodeno($nodeno);
+   return (defined $nodeno)?
+      grabSnippet($cfg->{"storypath"}.$nodeno.$cfg->{"storysuffix"}):
+      $cfg->{"text400"};
 }
 
 sub getPage {
-	my $nodeno = $_[1];
-	#add boilerplate
-	my $page;
-	#	add header
-	$page.=grabSnippet($cfg->{"boilerplatepath"}.$cfg->{"headername"});
-	#	add story
-	$page.=grabNode($nodeno);
-	#	add images
-	$page.='</div><div class="scene"><img src="images/"'.$bg.'"/><img src="images/"'.$mg.'"/><img src="images/".$fg.'"/>';
-	#	add footer
-	$page.=grabSnippet($cfg->{"boilerplatepath"}.$cfg->{"footername"});
-	
-	#return data
-	return $page;
+   my $nodeno = $_[1];
+   #add boilerplate
+   my $page;
+   #   add header
+   $page.=grabSnippet($cfg->{"boilerplatepath"}.$cfg->{"headername"});
+   #   get content
+   my $nodetext = grabNode($nodeno);
+        my $images = parseNode($nodetext);   
+   #   add story
+   $page.=grabNode($nodeno);
+   #   add images
+   $page.='</div><div class="scene"><img src="images/"';
+   $page.=$images->{"bg"};
+   $page.='"/><img src="images/"'.$images->{"mg"};
+   $page.='"/><img src="images/".$images->{"fg"}.'"/>';
+   #   add footer
+   $page.=grabSnippet($cfg->{"boilerplatepath"}.$cfg->{"footername"});
+   
+   #return data
+   return $page;
 }
 
 1;
--- spacecrabcfg.pm
+++ spacecrabcfg.pm
@@ -5,36 +5,53 @@ our $VERSION = '1.01';
 use base 'Exporter';
 our @EXPORT = qw(config);
 
-	my $storysuffix = '.node';
-	
-	my $cfg = {
-		#behaviour defaults
-		"startnode" => "node1",	
-		#finding files
-		"storypath" => "testdata/story/",
-		"boilerplatepath" => "boilerplate/",
-		"headername" => "header.html",
-		"footername" => "footer.html",
-		"storysuffix" => $storysuffix,
-		"spacecrab" => "spacecrab.pl",
-		"spacecrabmeat" => "spacecrabmeat.pl",
-		#node id constraints
-		"maxfnamelen" => 32,
-		#"nodepattern" => qr/^(\d+)($storysuffix)?$/, 
-		#"nodepattern" => qr/^node(\w+)($storysuffix)?$/, 
-		"nodepattern" => qr/^(node\w+)($storysuffix)?$/, 
-			#begins with one or more digits, 
-			#optionally followed by .node 
-			#but nothing else before ending
-		#player visible strings
-		"text400" => '<div class="story"><p>Hmm...That node isn\'t on our scanners.  Maybe it fell into a black hole?</p></div>',
-		#for tests
-		"testdata" => "testdata/",
-		"baseurl" => 'http://test.space-crab.com/'
+	my $nameparts = {
+	  "storysuffix" => '.node',
+	  "imagesuffix" => '.png',
+	  "storyprefix" => 'node',
+	  "foregroundprefix" => 'FG',
+          "midgroundprefix" => 'MG',
+	  "backgroundprefix" => 'BG'
 	};
 
+	my $paths = {
+            #basic paths
+		"storypath" => "testdata/story/",
+                "boilerplatepath" => "boilerplate/",
+                "imagepath" => "images/",
+            #for tests
+                "testdata" => "testdata/",
+                "baseurl" => 'http://test.space-crab.com/',
+            #files 
+		"headername" => "header.html",
+                "footername" => "footer.html",
+                "spacecrab" => "spacecrab.pl",
+                "spacecrabmeat" => "spacecrabmeat.pl"
+	};
+	
+	my $defaults = {
+	     #node id constraints
+                "maxfnamelen" => 32,
+                #"nodepattern" => qr/^($storyprefix\w+)($storysuffix)?$/, 
+                "nodepattern" => qr/^($nameparts->{"storyprefix"})(\w+)($nameparts->{"storysuffix"})?$/, 
+                        #begins with one or more digits,
+                        #optionally followed by .node
+                        #but nothing else before ending
+                #"imageidpattern" => qr/^([FMB]{1}G)(\w+)(\.png)/,
+                #"imageidpattern" => qr/^($nameparts->{"foregroundprefix"}|$nameparts->{"$midgroundprefix"}|$nameparts->{"backgroundprefix"})(\w+)($imagesuffix)/,
+             #player visible strings
+                "text400" => '<div class="story"><p>Hmm...That node isn\'t on our scanners.  Maybe it fell into a black hole?</p></div>',
+	     #default files
+		"startnode" => "node1",	
+                "fgdefault" => "FG1.png",
+                "mgdefault" => "MG1.png",
+                "bgdefault" => "BG1.png"
+	};
+	
+	my %cfg= (%$paths, %$defaults, %$nameparts); 
+
 	sub config {
-		return $cfg;
+		return \%cfg;
 	}
 	
 1;
