9e4ad87e3cda5c54060d81ac07d427bef0936b68
Dan <dchen12812@gmail.com>
2013-05-09 16:14:30 -0700

    Trying to fix save some more

--- boilerplate/spacecrabjuice.js
+++ boilerplate/spacecrabjuice.js
@@ -23,7 +23,7 @@ $(function()
 	}
 );
 });
-
+/*
 //Converted copier function to be in jQuery
 $(function copier() 
 {
@@ -79,12 +79,51 @@ $( "button.load" )
 .click(function() {
 loadSys();
 });
+*/
+
+//Save button which calls savesys
+$( "button.save" )
+.button()
+.click(function() {
+//Checks if browser supports this type of save
+	if(typeof(Storage)!=="undefined")
+		{
+		// Saves data in save
+				localStorage.setItem('saveCrab', ($('body').clone().html()));
+				
+				//Should tell what is actually in the savefile
+				alert( JSON.parse(JSON.stringify(localStorage.getItem('saveCrab'))));
+		}
+		// If browser dont support it, then can't save
+		else
+		  {
+				alert("Sorry, your browser does not support web storage");
+		  }
+});
+
+//Load button which loads
+$( "button.load" )
+.button()
+.click(function() {
+	//Checks if there is a save called saveCrab
+	if(localStorage.getItem('saveCrab') != null)
+		{
+		// Loads data from variable save and replaces the body
+			document.body.innerHTML = localStorage.getItem('saveCrab');
+		}
+		// If browser dont support it, then can't save/load
+	else
+	  {
+			alert("Sorry, no saves detected");
+	  }
+});
+
 
 //Should restart the game by calling spacecrab.pl?0
 $( "button.startOver" )
 .button()
 .click(function() {
-$.get('spacecrab.pl?0');
+$.load.get('spacecrab.pl?0');
 });
 
 //Builds the extra tabs
